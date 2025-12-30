8a315d8414b5e8f9608c7c10508576f51beb391e
Aslonov Davronbek <davronaslonov01@gmail.com>
2025-12-30 23:04:47 +0500

    commit

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -135,7 +135,7 @@ const Menu = () => {
                   <Link
                     href={item.href}
                     key={item.label}
-                    className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight"
+                    className="flex items-center justify-start lg:justify-start px-2 gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight"
                   >
                     <Image src={item.icon} alt="" width={20} height={20} />
                     <span className=" lg:block">{item.label}</span>
